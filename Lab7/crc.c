02b63727fc91f0a63bef1e4d1a6a370e30d8cfc7
MUDAYARAJ <92775506+MUDAYARAJ@users.noreply.github.com>
2023-01-05 00:17:02 +0530

    Update and rename crc.c.txt to crc.c

--- Lab7/crc.c
+++ Lab7/crc.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
  #include <conio.h>
  #include <string.h>
+ // C Program to concatenate two
+// strings without using strcat
+#include <stdio.h>
+
+
+ 
  void main() {
 int i,j,keylen,msglen;
 char input[100], key[30],temp[30],quot[100],rem[30],key1[30];
@@ -48,5 +54,42 @@ for (i=0;i<msglen;i++)
 printf("%c",input[i]);
 for (i=0;i<keylen-1;i++)
 printf("%c",rem[i]);
+
+for(i=0;i<(keylen);i++)
+input[msglen+i]=rem[i];
+
+keylen=strlen(key);
+msglen=strlen(input);
+strcpy(key1,key);
+for (i=0;i<keylen-1;i++) {
+input[msglen+i]='0';
+}
+for (i=0;i<keylen;i++)
+temp[i]=input[i];
+for (i=0;i<msglen;i++) {
+quot[i]=temp[0];
+if(quot[i]=='0')
+for (j=0;j<keylen;j++)
+key[j]='0'; 
+else
+for (j=0;j<keylen;j++)
+key[j]=key1[j];
+for (j=keylen-1;j>0;j--) {
+if(temp[j]==key[j])
+rem[j-1]='0'; else
+rem[j-1]='1';
+}
+rem[keylen-1]=input[i+keylen];
+strcpy(temp,rem);
+}
+strcpy(rem,temp);
+
+printf("%c",quot[i]);
+printf("\nRemainder is at reciever side ");
+for (i=0;i<keylen-1;i++)
+printf("%c",rem[i]);
+
+
+
 getch();
 }
